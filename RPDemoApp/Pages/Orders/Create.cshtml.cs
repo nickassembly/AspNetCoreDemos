937fed57d6b8ff6b64ba557324f0ace5a22172d6
Nick Guerra <nickg@maliciousgames.com>
2020-05-10 20:37:56 -0500

    add new foodItem class

---
+++ RPDemoApp/Pages/Orders/Create.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RPDemoApp.Pages.Orders
+{
+   public class CreateModel : PageModel
+   {
+      private readonly IFoodData _foodData;
+      private readonly IOrderData _orderData;
+
+      public List<SelectListItem> FoodItems { get; set; }
+
+      public CreateModel(IFoodData foodData, IOrderData orderData)
+      {
+         _foodData = foodData;
+         _orderData = orderData;
+      }
+
+      public void OnGet()
+      {
+
+      }
+
+
+   }
+}
