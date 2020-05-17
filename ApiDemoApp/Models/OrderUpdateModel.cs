ec8bbb1d7b7af251a4d6001733858597e96c5ce5
Nick Guerra <nickg@maliciousgames.com>
2020-05-17 12:58:29 -0500

    api demo project

---
+++ ApiDemoApp/Models/OrderUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDemoApp.Models
+{
+    public class OrderUpdateModel
+    {
+      public int Id { get; set; }
+
+      public string OrderName { get; set; }
+   }
+}
