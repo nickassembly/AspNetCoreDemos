e7eef61d48c4b38239c614bc01dbd227de2b50de
Nick Guerra <nickg@maliciousgames.com>
2020-05-17 15:34:56 -0500

    blazor project

---
+++ BlazorServerDemoApp/Models/OrderUpdateModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerDemoApp.Models
+{
+   public class OrderUpdateModel
+    {
+      public int Id { get; set; }
+
+      [DisplayName("Name for the Order")]
+      [MaxLength(20, ErrorMessage = "You need to keep the name to a max of 20 characters")]
+      [MinLength(3, ErrorMessage = "You need to enter at least 3 characters for an order name")]
+      [Required]
+      public string OrderName { get; set; }
+
+   }
+}
